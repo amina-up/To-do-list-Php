65a714a8c156a747d1b40843718fa5244c444cd3
Amina Laabidi <53490773+amina-up@users.noreply.github.com>
2021-06-01 11:18:03 +0200

    initialisation of project

---
+++ mytodo-app/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+    <div class="main-section">
+<div class="add-section">
+<form action="">
+  <input type="text" name="title" placeholder="this field is required"/>
+<button type="submit">Add &nbsp; <span>&#43;</span></button>
+</form>
+</div>
+<div class="show-todo-section">
+<div class="todo-item">
+<input type="checkbox">
+<h2>This is mine</h2>
+<br/>
+<small> created: 01/06/2021 </small>
+
+</div>
+
+</div>
+    </div>
+</body>
+</html>
